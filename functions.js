ff70eeee22d00a33a30a13ab2f99f8168ff1e98c
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-21 11:35:10 -0600

    Functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+// Name = "James"
+// console.log(Name)
+
+
+
+// - Ask the user to enter two numbers
+number = prompt("Enter two numbers")
+
+// Create a function that takes two numbers and retruns the sum
+function addNumbers(num1,num2)
+{
+return (num1 + num2)
+}
+// Print the sum of the numbers the user entered to the console using the function you created
+let userNumber1 = 
+let userNumber2 = 
+
+
+console.log(`The sum of ${userNumber1} and ${userNumber2} is ${addNumbers(userNumber1,userNumber2)}`)
+
+
+
+
+
+
+
